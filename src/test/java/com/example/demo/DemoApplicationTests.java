29987b84bf59488ef28e140cd37eb7fc0c412304
Francisco Márquez Chaves <francisco.marquez@dominion-global.com>
2019-02-13 09:42:17 +0100

    Added AssertJ and test to check that score is well Calculate

--- src/test/java/com/example/demo/DemoApplicationTests.java
+++ src/test/java/com/example/demo/DemoApplicationTests.java
@@ -1,5 +1,10 @@
 package com.example.demo;
 
+import java.util.List;
+
+import com.example.demo.Domain.AdDTO;
+
+import static org.assertj.core.api.Assertions.*;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -10,7 +15,63 @@ import org.springframework.test.context.junit4.SpringRunner;
 public class DemoApplicationTests {
 
     @Test
-    public void contextLoads() {
+    public void whenGetAllAdScoreThenAdWithId1ShouldHaveScoreNegative5() {
+        List<AdDTO> adDTOs = new AdminService().getAllAdScore();
+        AdDTO adDTO = getAdDTOById(adDTOs, 1);
+        assertThat(adDTO.getScore()).isEqualTo(-5);
+    }
+
+    @Test
+    public void whenGetAllAdScoreThenAdWithId2ShouldHaveScore35() {
+        List<AdDTO> adDTOs = new AdminService().getAllAdScore();
+        AdDTO adDTO = getAdDTOById(adDTOs, 2);
+        assertThat(adDTO.getScore()).isEqualTo(35);
+    }
+
+    @Test
+    public void whenGetAllAdScoreThenAdWithId3ShouldHaveScore20() {
+        List<AdDTO> adDTOs = new AdminService().getAllAdScore();
+        AdDTO adDTO = getAdDTOById(adDTOs, 3);
+        assertThat(adDTO.getScore()).isEqualTo(20);
+    }
+
+    @Test
+    public void whenGetAllAdScoreThenAdWithId4ShouldHaveScore70() {
+        List<AdDTO> adDTOs = new AdminService().getAllAdScore();
+        AdDTO adDTO = getAdDTOById(adDTOs, 4);
+        assertThat(adDTO.getScore()).isEqualTo(70);
+    }
+
+    @Test
+    public void whenGetAllAdScoreThenAdWithId5ShouldHaveScore35() {
+        List<AdDTO> adDTOs = new AdminService().getAllAdScore();
+        AdDTO adDTO = getAdDTOById(adDTOs, 5);
+        assertThat(adDTO.getScore()).isEqualTo(35);
+    }
+
+    @Test
+    public void whenGetAllAdScoreThenAdWithId6ShouldHaveScore50() {
+        List<AdDTO> adDTOs = new AdminService().getAllAdScore();
+        AdDTO adDTO = getAdDTOById(adDTOs, 6);
+        assertThat(adDTO.getScore()).isEqualTo(50);
+    }
+
+    @Test
+    public void whenGetAllAdScoreThenAdWithId7ShouldHaveScoreNegative5() {
+        List<AdDTO> adDTOs = new AdminService().getAllAdScore();
+        AdDTO adDTO = getAdDTOById(adDTOs, 7);
+        assertThat(adDTO.getScore()).isEqualTo(-5);
+    }
+
+    @Test
+    public void whenGetAllAdScoreThenAdWithId8ShouldHaveScore75() {
+        List<AdDTO> adDTOs = new AdminService().getAllAdScore();
+        AdDTO adDTO = getAdDTOById(adDTOs, 8);
+        assertThat(adDTO.getScore()).isEqualTo(75);
+    }
+
+    private AdDTO getAdDTOById(List<AdDTO> adDTOs, int id) {
+        return adDTOs.stream().filter(adDTO -> adDTO.getId() == id).findFirst().get();
     }
 
 }
